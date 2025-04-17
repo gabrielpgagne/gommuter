43fe1c308fe90da721d8727bad8e80e5d12210e4
Gabriel <gabrielpgagne@gmail.com>
2025-04-17 09:11:53 -0400

    half-hours only

--- web/dashboard.py
+++ web/dashboard.py
@@ -4,16 +4,15 @@ import os
 
 
 def load_commute_time(file):
-    try:
-        df = pd.read_csv(file, names=["datetime", "commute_time"])
-        df_dt = pd.to_datetime(df["datetime"])
-        df["hour_min"] = df_dt.dt.strftime("%H:%M")
-        df_weekday_nbr = df_dt.dt.weekday + 1
-        df_weekday_name = df_dt.dt.day_name()
-        df["weekday"] = df_weekday_nbr.astype(str) + " - " + df_weekday_name
-    except Exception as e:
-        print(e)
-        df = pd.DataFrame({"hour_min": [], "commute_time": []})
+    df = pd.read_csv(file, names=["datetime", "commute_time"])
+    df_dt = pd.to_datetime(df["datetime"], utc=True)
+    df_dt = df_dt.dt.tz_convert("US/Eastern")
+
+    df = df[df_dt.dt.minute.isin([0, 30])]
+    df["hour_min"] = df_dt.dt.strftime("%H:%M")
+    df_weekday_nbr = df_dt.dt.weekday + 1
+    df_weekday_name = df_dt.dt.day_name()
+    df["weekday"] = df_weekday_nbr.astype(str) + " - " + df_weekday_name
     return df
 
 
@@ -44,64 +43,69 @@ def make_tab(id):
     col1, col2 = st.columns(2, border=True)
     with col1:
         st.header("Commute time - to")
-        df = load_commute_time(f"data/to-{id}.csv")
+        try:
+            df = load_commute_time(f"data/to-{id}.csv")
+        except Exception as e:
+            print(e)
+        else:
+            st.subheader("Average commute time")
+            adf = get_average_commute_time(df)
+            adf["+/-"] = df.groupby("hour_min")["commute_time"].std().values
+            st.bar_chart(
+                adf,
+                x="hour_min",
+                y="commute_time",
+                x_label="Departure time (HH:MM)",
+                y_label="Average commute time (min)",
+                color="+/-",
+            )
 
-        st.subheader("Average commute time")
-        adf = get_average_commute_time(df)
-        adf["+/-"] = df.groupby("hour_min")["commute_time"].std().values
-        st.bar_chart(
-            adf,
-            x="hour_min",
-            y="commute_time",
-            x_label="Departure time (HH:MM)",
-            y_label="Average commute time (min)",
-            color="+/-",
-        )
-
-        st.subheader("Average day-wise commute time")
-        adf = get_average_commute_time_daywise(df)
-        st.bar_chart(
-            adf,
-            x="hour_min",
-            y="commute_time",
-            x_label="Departure time (HH:MM)",
-            y_label="Average commute time (min)",
-            stack=False,
-            color="weekday",
-        )
-    if st.checkbox(f"Show 'to-{id}' data"):
-        st.dataframe(df)
+            st.subheader("Average day-wise commute time")
+            adf = get_average_commute_time_daywise(df)
+            st.bar_chart(
+                adf,
+                x="hour_min",
+                y="commute_time",
+                x_label="Departure time (HH:MM)",
+                y_label="Average commute time (min)",
+                stack=False,
+                color="weekday",
+            )
+            if st.checkbox(f"Show 'to-{id}' data"):
+                st.dataframe(df)
 
     with col2:
         st.header("Commute time - from")
-        df = load_commute_time(f"data/from-{id}.csv")
+        try:
+            df = load_commute_time(f"data/from-{id}.csv")
+        except Exception as e:
+            print(e)
+        else:
+            st.subheader("Average commute time")
+            adf = get_average_commute_time(df)
+            adf["+/-"] = df.groupby("hour_min")["commute_time"].std().values
+            st.bar_chart(
+                adf,
+                x="hour_min",
+                y="commute_time",
+                x_label="Departure time (HH:MM)",
+                y_label="Average commute time (min)",
+                color="+/-",
+            )
 
-        st.subheader("Average commute time")
-        adf = get_average_commute_time(df)
-        adf["+/-"] = df.groupby("hour_min")["commute_time"].std().values
-        st.bar_chart(
-            adf,
-            x="hour_min",
-            y="commute_time",
-            x_label="Departure time (HH:MM)",
-            y_label="Average commute time (min)",
-            color="+/-",
-        )
-
-        st.subheader("Average day-wise commute time")
-        adf = get_average_commute_time_daywise(df)
-        st.bar_chart(
-            adf,
-            x="hour_min",
-            y="commute_time",
-            x_label="Departure time (HH:MM)",
-            y_label="Average commute time (min)",
-            stack=False,
-            color="weekday",
-        )
-
-    if st.checkbox(f"Show 'from-{id}' data"):
-        st.dataframe(df)
+            st.subheader("Average day-wise commute time")
+            adf = get_average_commute_time_daywise(df)
+            st.bar_chart(
+                adf,
+                x="hour_min",
+                y="commute_time",
+                x_label="Departure time (HH:MM)",
+                y_label="Average commute time (min)",
+                stack=False,
+                color="weekday",
+            )
+            if st.checkbox(f"Show 'from-{id}' data"):
+                st.dataframe(df)
 
 
 # Run the app
